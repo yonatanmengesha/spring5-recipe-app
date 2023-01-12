88c2574ca47e72c6db651a50731f42e6ecf05e86
Yonatan Mengesha <yonatan.mengesha2017@gmail.com>
2023-01-12 10:54:10 -0500

    created a branch  database-initialization and added data.sql

--- src/main/java/guru/springframework/spring5recipeapp/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/spring5recipeapp/Spring5RecipeAppApplication.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5recipeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+//@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class Spring5RecipeAppApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/guru/springframework/spring5recipeapp/repository/UnitOfMeasureRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.spring5recipeapp.repository;
+
+import guru.springframework.spring5recipeapp.domain.UnitOFMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOFMeasure,Long> {
+}
