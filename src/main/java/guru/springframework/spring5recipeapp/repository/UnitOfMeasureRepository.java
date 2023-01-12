67b6c0a3a6156e0b8ba81468dea523b95f908ae2
Yonatan Mengesha <yonatan.mengesha2017@gmail.com>
2023-01-12 11:11:37 -0500

    created a branch  database-initialization and added data.sql

---
+++ src/main/java/guru/springframework/spring5recipeapp/repository/UnitOfMeasureRepository.java
@@ -0,0 +1,14 @@
+package guru.springframework.spring5recipeapp.repository;
+
+
+import guru.springframework.spring5recipeapp.domain.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+/**
+ * Created by jt on 6/13/17.
+ */
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+}
